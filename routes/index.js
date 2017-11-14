01ca52bf2dcb0a400950b522c308fbff416a2dce
Ville Kumpulainen <ville.kumpulainen@relexsolutions.com>
2017-11-14 15:58:42 +0200

    Homepage country list working as specified

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
+
+var options = {
+    uri: 'https://restcountries.eu/rest/v2/all',
+    json: true
+};
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    rp(options)
+        .then( (countries) => {
+            res.render('index', { countrydata: countries});
+        }).catch( (error) => {
+            console.log(error);
+        });
 });
 
 module.exports = router;
