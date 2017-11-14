870d3404545d703899cd4a2fa9282211bdfea7bc
coumbole <ville.kumpulainen@gmail.com>
2017-11-14 20:05:02 +0200

    Detail page working as specified

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,64 @@
-var express = require('express');
-var router = express.Router();
-var rp = require('request-promise');
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
 
-var options = {
-    uri: 'https://restcountries.eu/rest/v2/all',
-    json: true
+// Country API
+const api = 'https://restcountries.eu/rest/v2/';
+
+// API request options
+const options = {
+    uri : api + 'all',
+    json : true
+};
+
+const getCountryData = (options) => {
+    return rp(options);
 };
 
 
-/* GET home page. */
+// GET home page
 router.get('/', (req, res, next) => {
-    rp(options)
-        .then( (countries) => {
-            res.render('index', { countrydata: countries});
-        }).catch( (error) => {
-            console.log(error);
-        });
+
+    // Fetch data, if successfull, render page
+    getCountryData(options).then( (data) => {
+        res.render('index', { data: data});
+    }).catch( (error) => {
+        console.log(error);
+    });
 });
+
+
+// GET detail page
+router.get('/country/:id/', (req, res, next) => {
+
+    // ISO 3166-1 3 letter country code
+    const code = req.params.id.toUpperCase();
+
+    // Fetch countrydata
+    getCountryData(options)
+
+        // On success, handle the data
+        .then( (countries) => {
+
+            // Get the chosen country's data from the array
+            const thecountry = countries.find( (country) => {
+                return country.alpha3Code === code;
+            });
+
+            // Get the neighbouring countries' data in json format
+            const neighbours = countries.filter( (country) => {
+                return thecountry.borders.includes(country.alpha3Code);
+            });
+
+            // Render the page with all the details
+            res.render('detail', { current: thecountry, data: countries, borders: neighbours });
+
+        // Handle errors
+            //// Handle errors
+        }).catch( (error) => {
+            res.render('detail');
+        });
+
+})
 
 module.exports = router;
